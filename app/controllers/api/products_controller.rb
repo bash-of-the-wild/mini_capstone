b888ccd7ecf93a41ce130443d65912e3524f2ee2
JackMarx <seriousfools@gmail.com>
2019-05-02 10:56:21 -0500

    all products and individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,21 @@
 class Api::ProductsController < ApplicationController
+  def all_products
+    @products = Product.all
+    render 'all_products.json.jbuilder'
+  end
+
+  def first_product
+    @product = Product.first
+    render 'first_product.json.jbuilder'
+  end
+
+  def second_product
+    @product = Product.find(2)
+    render 'second_product.json.jbuilder'
+  end
+
+  def third_product
+    @product = Product.find(3)
+    render 'third_product.json.jbuilder'
+  end
 end
