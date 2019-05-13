d969ad66b691c95794d996176540a9dc82027d8d
JackMarx <seriousfools@gmail.com>
2019-05-13 12:28:45 -0500

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,14 +8,16 @@ class Product < ApplicationRecord
   # validates :description, length: { in: 50..200 }
   validates :description, presence: true
 
-  def images
-    Image.where(product_id: self.id)
-  end
+  has_many :images
+  # def images
+  #   Image.where(product_id: self.id)
+  # end
 
-  def supplier
-    # the purpose of this method is to return the Supplier Object associated with this Product
-    Supplier.find_by(id: self.supplier_id)
-  end
+  belongs_to :supplier
+  # def supplier
+  #   # the purpose of this method is to return the Supplier Object associated with this Product
+  #   Supplier.find_by(id: self.supplier_id)
+  # end
 
   def is_discounted?
     price < 45
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,7 +1,3 @@
 class Supplier < ApplicationRecord
-
-  def products
-    # the purpose this method is to return a collection all Products associated with this Supplier
-    Product.where(supplier_id: self.id)
-  end
+  has_many :products
 end
