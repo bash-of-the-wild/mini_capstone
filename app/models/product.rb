c6f1e6264a09fd78e0ed6573da91852087c8ae6f
JackMarx <seriousfools@gmail.com>
2019-05-15 11:25:34 -0500

    order associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,16 +8,10 @@ class Product < ApplicationRecord
   # validates :description, length: { in: 50..200 }
   validates :description, presence: true
 
+  has_many :orders
   has_many :images
-  # def images
-  #   Image.where(product_id: self.id)
-  # end
-
   belongs_to :supplier
-  # def supplier
-  #   # the purpose of this method is to return the Supplier Object associated with this Product
-  #   Supplier.find_by(id: self.supplier_id)
-  # end
+
 
   def is_discounted?
     price < 45
