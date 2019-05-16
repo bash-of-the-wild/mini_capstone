8ace4444b349d5201868ff15eb9a5fd748b54068
JackMarx <seriousfools@gmail.com>
2019-05-16 10:04:52 -0500

    admin

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,27 +1,18 @@
 class Api::OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def index
-    if current_user
-      @orders = current_user.orders
-      render 'index.json.jbuilder'
-    else
-      render json: []
-    end
+    @orders = current_user.orders
+    render 'index.json.jbuilder'
   end
 
   def create
-    product = Product.find(params[:product_id])
-    calculated_subtotal = params[:quantity].to_i * product.price
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
-
     @order = Order.new(
                        user_id: current_user.id,
                        product_id: params[:product_id],
-                       quantity: params[:quantity],
-                       subtotal: calculated_subtotal,
-                       tax: calculated_tax,
-                       total: calculated_total
+                       quantity: params[:quantity]
                       )
+    @order.store_totals
 
     if @order.save
       render 'show.json.jbuilder'
